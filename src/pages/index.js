285f73a00b0121537041d77406905709cf662f2e
Kartik <kartik@hyperverge.co>
2021-05-16 14:38:05 +0530

    Update: title info

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || `Kartik's portfolio`}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || `Kartik's portfolio`} />
       </Helmet>
       <App />
     </>
